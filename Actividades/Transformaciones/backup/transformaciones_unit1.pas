fa871df42416f76639eb19ebdf68e7dd9e07878a
raulminan <88097814+raulminan@users.noreply.github.com>
2022-02-20 13:39:25 +0100

    Add files via upload

---
+++ Actividades/Transformaciones/backup/transformaciones_unit1.pas
@@ -0,0 +1,33 @@
+unit transformaciones_unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  biotoolkit;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+    Memo1: TMemo;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ Actividades/Transformaciones/transformaciones_unit1.pas
@@ -0,0 +1,34 @@
+unit transformaciones_unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  biotoolkit;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Image1: TImage;
+    Image2: TImage;
+    Memo1: TMemo;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
